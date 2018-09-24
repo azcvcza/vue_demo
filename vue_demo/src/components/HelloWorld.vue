5d95c1384f12641b986887482f6c0a21c57e915b
azcvcza <283940802@qq.com>
2018-09-24 22:29:11 +0800

    feature: add lifecycle log

--- vue_demo/src/components/HelloWorld.vue
+++ vue_demo/src/components/HelloWorld.vue
@@ -2,18 +2,53 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <input type="text" v-model="message">
+    <button v-on:click="console">click me to log</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data : () => {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      message:"input your answer"
+      msg : 'Welcome to Your Vue.js App',
+      message : 'input your answer'
     }
+  },
+  methods:{
+    console : function () {
+      console.log('button click')
+    }
+  },
+  init: function () {
+    console.log('init');
+  },
+  created: function () {
+    console.log('created');
+  },
+  beforeCompile: function () {
+    console.log('before complied')
+  },
+  compiled: function () {
+    console.log('complied')
+  },
+  attached: function () {
+    console.log('attached')
+  },
+  dettached: function () {
+    console.log('dettached')
+  },
+  beforeDestroyed: function () {
+    console.log('beforeDestroyed')
+  },
+  destroyed: function () {
+    console.log('destroyed')
+  },
+  ready: function ()　{
+    console.log('ready');
+    this.$destroy();
   }
+
 }
 </script>
 
